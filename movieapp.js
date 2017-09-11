86896bb9c77f14570e6f8722a86e18c6690c74f7
biswarup <bis.banerjee.bb@gmail.com>
2017-09-11 19:09:55 +0530

    done with basic movie add

--- movieapp.js
+++ movieapp.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
     $('#add_movie').click(function(){
-        $("#movie_added").load("added_movie.txt");
+      // $( "#movie_added" ).append( "<p>Test</p>" );
+
+      // $("#movie_added").append.load("added_movie.html");
+      // $("movie_added").load("added_movie.html #posts", function() {
+      // $("#hi").append($(this).find("#posts").html());
+      // $('#movie_added').append($('<div>').load('added_movie.html'));
+      var image = document.getElementById('movie_poster').value;
+      console.log("Image url: " +  image);
+      // $("#poster_image").attr("src",image);
+      // console.log(image);
+
+
+      $( "#movie_added" ).append( '<div class="col m4"> <div class="card"> <div class="card-image"> <img src='   + image +   ' id="poster_image_'+'1'+    '> <span class="card-title"><b>Raees</b></span> </div> <div class="card-content"> <h6>Raees</h6> </div> <div class="card-action"> <a href="#">This is a link</a> </div> </div> </div>' );
+
     });
 });
