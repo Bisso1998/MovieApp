e6215051a1ae767a1ee5fa91514ae3cfe1f8bdd2
biswarup <bis.banerjee.bb@gmail.com>
2017-09-11 20:02:34 +0530

    added remove option

--- movieapp.js
+++ movieapp.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  var i=0;
+  var k=0;
     $('#add_movie').click(function(){
       // $( "#movie_added" ).append( "<p>Test</p>" );
 
@@ -13,7 +15,13 @@ $(document).ready(function(){
       console.log("Image url: " +  image);
       // $("#poster_image").attr("src",image);
       // console.log(image);
-      $( "#movie_added" ).append( '<div class="col m4"> <div class="card"> <div class="card-image"> <img src='   + image +   ' id="poster_image_'+'1'+    '> <span class="card-title"><b></b></span> </div> <div class="card-content"> <h6>'+ movie_title +  '</h6> </div> <div class="card-action"> <a href="#">'  + movie_genre +    '</a> </div> </div> </div>' );
-
+      $( "#movie_added" ).append( '<div class="col m4" id=C'+(k)+' > <div class="card"> <div class="card-image"> <img src='   + image +   ' id="poster_image_'+ (i++) +    '> <span class="card-title"><b></b></span> </div> <div class="card-content"> <h6>'+ movie_title +  '</h6> </div> <div class="card-action"> <a href="#">'  + movie_genre +    '</a>           <a class="waves-effect waves-light btn blue" id="doneCounter" onclick="hide('+k+')">done</a> </div> </div> </div>' );
+      k++;
     });
 });
+
+function hide(k)
+{
+  console.log(k);
+  $('#C'+k).remove();
+}
